2bdf9950f5bee92e018889c273d99a1496786019
ruwan9 <ruwan9@kakao.com>
2021-08-31 22:55:50 +0900

    20210831

--- Baekjoon/IM/1244.py
+++ Baekjoon/IM/1244.py
@@ -75,39 +75,3 @@ ans = onoff[1:]
 
 for i in range(0, n, 20):
     print(*ans[i:i+20])
-
-
-# switch_num = int(input())
-# onoff = [0]
-# onoff.extend(list(map(int, input().split())))
-# student_num = int(input())
-# students = [list(map(int, input().split())) for _ in range(student_num)]
-
-# for student in students:
-#     gender, switch = student[0], student[1]
-#     if gender == 1:
-#         for i in range(len(onoff)):
-#             if i % switch == 0 and onoff[i] == 0:
-#                 onoff[i] = 1
-#             elif i % switch == 0 and onoff[i] == 1:
-#                 onoff[i] = 0
-
-#     elif gender == 2:
-#         onoff = onoff[1:]
-#         print(onoff)
-#         i = 0
-#         while onoff[(switch-1)-i] == onoff[(switch-1)+i] and 0<= switch-1-i < switch_num:
-#             if  onoff[switch-1-i] == 0:
-#                 onoff[switch-1-i] = 1
-#                 onoff[switch-1+i] = 1
-#                 i += 1
-#             elif onoff[switch-1-i] == 1:
-#                 onoff[switch-1-i] = 0
-#                 onoff[switch-1+i] = 0
-#                 i += 1
-
-# if len(onoff) >= 20:
-#     for i in range(20):
-#         print(onoff[i], end=' ')
-#     print()
-#     print(*onoff[20:])
